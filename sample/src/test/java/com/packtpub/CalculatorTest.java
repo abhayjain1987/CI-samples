1d1e39aa1f7bfd6d01310923435c953ed09229b5
jainabhay <abhayjain1987@gmail.com>
2016-03-21 23:12:16 +0800

    Testing Continuous Integration VCS trigger setup

--- sample/src/test/java/com/packtpub/CalculatorTest.java
+++ sample/src/test/java/com/packtpub/CalculatorTest.java
@@ -15,4 +15,10 @@ public class CalculatorTest extends TestCase {
         Assert.assertEquals(3, sum);
 
     }
+
+    public void testBad() throws Exception {
+
+        Assert.fail("Some bad test has failed");
+
+    }
 }
